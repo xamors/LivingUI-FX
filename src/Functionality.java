9f4462d4b23982fd6c5d581eca236769d62a2f30
Xamors <joestuder1@gmail.com>
2018-12-24 21:33:00 +0000

    Skills Commit

--- src/Functionality.java
+++ src/Functionality.java
@@ -12,8 +12,8 @@ import javafx.stage.Stage;
 
 public class Functionality extends Frame {
 
-    TextField Name,Strength,Dexteriy,Intelligence,Constitution,Wisdom,Charisma;
-    Label NameL, StrengthL;
+    TextField Name,Strength,Dexterity,Intelligence,Constitution,Wisdom,Charisma;
+    Label NameL, StrengthL,DexterityL,IntelligenceL,ConstitutionL,WisdomL,CharismaL;
 
     public void Components(){
 
@@ -21,15 +21,46 @@ public class Functionality extends Frame {
         Name = new TextField("Name");
         StrengthL = new Label("Strength");
         Strength = new TextField("Strength");
+        DexterityL = new Label("Dexterity");
+        Dexterity = new TextField("Dexterity");
+        IntelligenceL = new Label("Intelligence");
+        Intelligence = new TextField("Intelligence");
+        ConstitutionL = new Label("Constitution");
+        Constitution = new TextField("Constitution");
+        WisdomL = new Label("Wisdom");
+        Wisdom = new TextField("Wisdom");
+        CharismaL = new Label("Wisdom");
+        Charisma = new TextField("Wisdom");
+
         layout.add(NameL,0,0);
         layout.add (Name, 1,0);
         layout.add(StrengthL,0,1);
         layout.add (Strength, 1,1);
-        GridPane.setMargin(Name, new Insets(10,10,10,10));
+        layout.add(DexterityL,0,2);
+        layout.add (Dexterity, 1,2);
+        layout.add(IntelligenceL,0,3);
+        layout.add (Intelligence, 1,3);
+        layout.add(ConstitutionL,0,4);
+        layout.add (Constitution, 1,4);
+        layout.add(WisdomL,0,5);
+        layout.add (Wisdom, 1,5);
+        layout.add(CharismaL,0,5);
+        layout.add (Charisma, 1,5);
+
+        GridPane.setMargin(Name, new Insets(10,10,10,15));
         GridPane.setMargin(NameL, new Insets(0,0,0,10));
         GridPane.setMargin(StrengthL, new Insets(0,0,0,10));
         GridPane.setMargin(Strength, new Insets(0,0,0,15));
-
+        GridPane.setMargin(DexterityL, new Insets(5,0,0,10));
+        GridPane.setMargin(Dexterity, new Insets(10,0,0,15));
+        GridPane.setMargin(IntelligenceL, new Insets(5,0,0,10));
+        GridPane.setMargin(Intelligence, new Insets(10,0,0,15));
+        GridPane.setMargin(ConstitutionL, new Insets(5,0,0,10));
+        GridPane.setMargin(Constitution, new Insets(10,0,0,15));
+        GridPane.setMargin(WisdomL, new Insets(5,0,0,10));
+        GridPane.setMargin(Wisdom, new Insets(10,0,0,15));
+        GridPane.setMargin(CharismaL, new Insets(5,0,0,10));
+        GridPane.setMargin(Charisma, new Insets(10,0,0,15));
     }
 
 }
