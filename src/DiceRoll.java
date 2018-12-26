2bef7ff302809847aa0585e97fc3bf64e735db3c
Xamors <joestuder1@gmail.com>
2018-12-26 23:24:41 +0000

    Dice Roll working

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,12 +1,47 @@
-import javafx.scene.control.CheckBox;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import java.util.Random;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.image.Image ;
+import java.awt.*;
 
-public class DiceRoll {
+//public class DiceRoll extends Functionality {
+//
+//
+//    public void initialize() {
+//
+//        RollB.setOnAction(e -> {
+//
+//            if (D4.isSelected()) {
+//                NumDice = Integer.valueOf(D4L.getText());
+//            }
+//
+//            while (MinDice <= NumDice) {
+//                Random rand = new Random();
+//                GenNum = rand.nextInt(4) + 1;
+//                total += GenNum;
+//                MinDice++;
+//            }
+//
+//            RollT.setText("" + total);
+//
+//            MinDice = 1;
+//            NumDice = 0;
+//            total = 0;
+//
+//        });
+//    }
+//}
 
 
 
-public static void DicePos(){
 
-
-
-    }
-}
